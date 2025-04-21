3fcf78e5dd37b4c56227b0984cd20db38679ee39
clm302002 <clm302002@gmail.com>
2025-04-20 18:17:13 -0600

    reversed row pins, SOLVED THE KEYMAP!

--- keymaps/mytest/keymap.c
+++ keymaps/mytest/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,  KC_F1,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,  KC_F1,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_F1, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,                    KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
+                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,                    KC_ENT, KC_BSPC, KC_RGUI, MO(_RAISE)
 ),
 
 
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,  KC_F1,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,  KC_F1,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_F1, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,                    KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
+                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,                    KC_ENT, KC_BSPC, KC_RGUI, MO(_RAISE)
 ),
 
  [_RAISE] = LAYOUT(
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,  KC_F1,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,  KC_F1,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_F1, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,                    KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
+                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,                    KC_ENT, KC_BSPC, KC_RGUI, MO(_RAISE)
 ),
 
  [_ADJUST] = LAYOUT(
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,  KC_F1,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,  KC_F1,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_F1, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,                    KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
+                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,                    KC_ENT, KC_BSPC, KC_RGUI, MO(_RAISE)
 ),
 };
 
